78a335e0f5b72bdbfa917a476738ad0a2692189b
Mrbrutal <jost.bizjak@gmail.com>
2012-08-25 20:06:17 +0200

    Switched workspaces.

---
+++ CalcVersion.py
@@ -0,0 +1,104 @@
+import time
+import os
+import sys
+import commands
+import datetime
+import fnmatch
+import subprocess, shlex
+import re
+
+file_location = os.getcwd()
+prop_file = file_location + "\lcver.properties"
+list = []
+
+major = "1"
+minor = "0"
+rev = "0"
+sub = "000"
+mcpversion = "7.2"
+forgeversion = "4.0.0.217"
+mcclientver = "1.3.2"
+
+def cmdsplit(args):
+    if os.sep == '\\':
+        args = args.replace('\\', '\\\\')
+    return shlex.split(args)
+
+def file_exists(filename):
+    try:
+        with open(filename) as f:
+            return True
+    except IOError:
+        return False
+    
+def calc():
+    global major
+    global minor
+    global rev
+    global sub
+    global mcpversion
+    global mcclientver
+    
+    major = list[0]
+    minor =  list[1]
+    rev =  list[2]
+    sub = list[3]
+    subver = int(sub)
+    subver += 1
+    sub = str(subver).zfill(3)
+    mcpversion = list[4]
+    mcclientver = list[5]
+
+def read():
+    with open(prop_file,"r") as f:
+        lines = f.readlines()        
+        for line in lines:
+            line = line.strip()
+            if not '#' in line:
+                split = line.split('=')
+                list.append(split[1])
+        f.close()
+
+def write():
+    date = time.asctime(time.localtime(time.time()))
+    with open("lcver.properties","w+") as f:
+        f.write("# %s \n" %(date))
+        f.write("%s=%s\n" %("lc.build.major.number",major))
+        f.write("%s=%s\n" %("lc.build.minor.number",minor))
+        f.write("%s=%s\n" %("lc.build.revision.number",rev))
+        f.write("%s=%s\n" %("lc.build.subversion.number",sub))
+        f.write("%s=%s\n" %("lc.build.mcpversion",mcpversion))
+        f.write("%s=%s\n" %("lc.build.forgeversion",forgeversion))
+        f.write("%s=%s\n" %("lc.build.mcclientversion",mcclientver))
+        f.write("%s=%s.%s.%s_%s\n" %("lc.build.build",major, minor, rev, sub))
+        f.close()
+        
+def getGit(): 
+    print("[PY]Obtaining version information from github")
+    comLine = "git describe --tags"     
+    try:
+      p = subprocess.Popen(comLine, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=-1)
+      ver, _ = p.communicate()
+    except OSError:
+      print("Git not found")
+      ver="1.0.0"
+      (major,minor,rev)=re.match("(\d+).(\d+).(\d+)",ver).groups()
+      print("[PY]Version information from git: Logisticraft %s.%s.%s_%s using MCP %s for c:%s" % (major, minor, rev, sub, mcpversion, mcclientver))
+
+def main():
+    print("[PY]Obtaining version information for mod")
+    if file_exists(prop_file):
+        print("[PY]Properties file exists: reading...")
+        read()
+        calc()
+        write()
+    else:
+        print("[PY]Properties file does not exists: writting...")
+        #getGit()
+        write()
+        
+    print("[PY]"+time.asctime(time.localtime(time.time())))
+    print("[PY]Version information: Logisticraft %s.%s.%s_%s using MCP %s and Forge %s for c:%s" % (major, minor, rev, sub, mcpversion, forgeversion, mcclientver))
+    
+if __name__ == '__main__':
+    main()
---
+++ Common/si/meansoft/logisticraft/common/Logisticraft.java
@@ -0,0 +1,41 @@
+package si.meansoft.logisticraft.common;
+
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+@Mod(modid = "LC", name = "Logisticraft", version = "1.0.0")
+@NetworkMod(channels = { "Logisticraft" }, clientSideRequired = true, serverSideRequired = true, packetHandler = PacketHandler.class)
+public class Logisticraft {
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+
+	@Init
+	public void load(FMLInitializationEvent evt) {
+		
+	}
+
+	@PostInit
+	public void modsLoaded(FMLPostInitializationEvent evt) {
+		//All the mod checking here
+	}
+}
