7d80d99c0520e4bf1304eddc6e900328d633cea1
Mrbrutal <jost.bizjak@gmail.com>
2012-10-13 01:43:31 +0200

    Removed now unussed naming.

--- Common/si/meansoft/logisticraft/common/blocks/LCBlocks.java
+++ Common/si/meansoft/logisticraft/common/blocks/LCBlocks.java
@@ -32,103 +32,61 @@ import net.minecraft.src.Material;
 import net.minecraft.src.ModLoader;
 
 public class LCBlocks {
-	
-	public static Block ores;
-	public static Block box;
-	public static Block box2;
-	public static Block crate;
-	public static Block crate2;
-	public static Block playerPlate;
-	public static Block coloredGlass;
-	public static Block machines;
-	public static Block beam;
-	public static Block chimney;
-	public static Block stackBench;
-	
-	/* Block declarations */
-	public static void loadBlocks() {
-		ores = new BlockOres(BlockIDs.ores, 3);
-		box = new BlockBox(BlockIDs.box, 0);
-		box2 = new BlockBox(BlockIDs.box2, 16);
-		crate = new BlockCrate(BlockIDs.crate, 0);
-		crate2 = new BlockCrate(BlockIDs.crate2, 16);
-		playerPlate = new BlockPlayerPlate(BlockIDs.playerPlate, 0, EnumMobType.players, Material.rock).setHardness(0.5F);
-		coloredGlass = new BlockColorGlass(BlockIDs.coloredGlass, 240, Material.glass);
-		machines = new BlockMachines(BlockIDs.machines, 24, false);
-		beam = new BlockColorGlass(BlockIDs.beam, 240-16, Material.vine).setLightOpacity(0).setLightValue(1.0F);
-		chimney = new BlockChimney(BlockIDs.chimney, 32);
-		stackBench = new BlockStackBench(BlockIDs.stackBench, 20);
-	}
-	
-	/* Block registration */
-	public static void registerBlocks() {
-		GameRegistry.registerBlock(playerPlate);
-		GameRegistry.registerBlock(stackBench);
-		GameRegistry.registerBlock(machines, ItemBlockMachines.class);
-		GameRegistry.registerBlock(box, ItemBlockBox.class);
-		GameRegistry.registerBlock(box2, ItemBlockBox2.class);
-		GameRegistry.registerBlock(crate, ItemBlockCrate.class);
-		GameRegistry.registerBlock(crate2, ItemBlockCrate2.class);
-		GameRegistry.registerBlock(ores, ItemBlockOres.class);
-		GameRegistry.registerBlock(coloredGlass, ItemBlockGlass.class);
-		GameRegistry.registerBlock(beam, ItemBlockGlass.class);
-		GameRegistry.registerBlock(chimney, ItemBlockChimney.class);
-	}
-	
-	/* Block names and translations */
-	public static void nameBlocks() {
-		playerPlate.setBlockName("pressurePlate");
-		stackBench.setBlockName("Stackbench");
-		ores.setBlockName("ores");
-		box.setBlockName("box");
-		box2.setBlockName("box");
-		crate.setBlockName("crate");
-		crate2.setBlockName("crate");
-		machines.setBlockName("machines");
-		coloredGlass.setBlockName("coloredGlass");
-		beam.setBlockName("beam");
-		chimney.setBlockName("Chimney");
-		
-		addNames(Info.crateNames, "Crated ", crate, true);
-		addNames(Info.crateNames2, "Crated ", crate2, true);
-		addNames(Info.glassNames, " glass", coloredGlass, false);
-		addNames(Info.crateNames, "Box of ", box, true);
-		addNames(Info.crateNames2, "Box of ", box2, true);
-		
-		LanguageRegistry.addName(playerPlate, "Player preasure plate");
-		
-		LanguageRegistry.addName(new ItemStack(ores, 1, 0), "Platinum ore");
-		LanguageRegistry.addName(new ItemStack(ores, 1, 1), "Silver ore");
-		LanguageRegistry.addName(new ItemStack(ores, 1, 2), "Copper ore");
-		
-		LanguageRegistry.addName(new ItemStack(machines, 1, 0), "Lava remover");
-		LanguageRegistry.addName(new ItemStack(machines, 1, 1), "Lava remover on");
-		LanguageRegistry.addName(new ItemStack(machines, 1, 2), "Lava remover block");
-		LanguageRegistry.addName(new ItemStack(machines, 1, 3), "Time setter");
-		LanguageRegistry.addName(new ItemStack(machines, 1, 4), "Reflector");
-		LanguageRegistry.addName(new ItemStack(machines, 1, 5), "Reflector on");
-		
-		LanguageRegistry.addName(new ItemStack(chimney, 1, 0), "Chimney");
-		LanguageRegistry.addName(new ItemStack(chimney, 1, 1), "Chimney top");
-		LanguageRegistry.addName(new ItemStack(chimney, 1, 2), "Crate");
-		LanguageRegistry.addName(new ItemStack(chimney, 1, 3), "Box");
-		LanguageRegistry.addName(new ItemStack(chimney, 1, 4), "Rotten crate");
-		LanguageRegistry.addName(new ItemStack(chimney, 1, 5), "Rotten box");
-		
-		LanguageRegistry.addName(stackBench, "Stackbench");
-	}
-	
-	/* Format names for meta blocks */
-	public static void addNames(String[] names, String name, Block block, boolean side) {
-		if(side) {
-			for (int i = 0; i < names.length; i++) {
-				LanguageRegistry.addName(new ItemStack(block , 1, i), name + names[i]);
-			}
-		}
-		else {
-			for (int i = 0; i < names.length; i++) {
-				LanguageRegistry.addName(new ItemStack(block , 1, i), names[i] + name);
-			}
-		}
-	}
+
+    public static Block ores;
+    public static Block box;
+    public static Block box2;
+    public static Block crate;
+    public static Block crate2;
+    public static Block playerPlate;
+    public static Block coloredGlass;
+    public static Block machines;
+    public static Block beam;
+    public static Block chimney;
+    public static Block stackBench;
+
+    /* Block declarations */
+    public static void loadBlocks() {
+	ores = new BlockOres(BlockIDs.ores, 3);
+	box = new BlockBox(BlockIDs.box, 0);
+	box2 = new BlockBox(BlockIDs.box2, 16);
+	crate = new BlockCrate(BlockIDs.crate, 0);
+	crate2 = new BlockCrate(BlockIDs.crate2, 16);
+	playerPlate = new BlockPlayerPlate(BlockIDs.playerPlate, 0, EnumMobType.players, Material.rock).setHardness(0.5F);
+	coloredGlass = new BlockColorGlass(BlockIDs.coloredGlass, 240, Material.glass);
+	machines = new BlockMachines(BlockIDs.machines, 24, false);
+	beam = new BlockColorGlass(BlockIDs.beam, 240 - 16, Material.vine).setLightOpacity(0).setLightValue(1.0F);
+	chimney = new BlockChimney(BlockIDs.chimney, 32);
+	stackBench = new BlockStackBench(BlockIDs.stackBench, 20);
+    }
+
+    /* Block registration */
+    public static void registerBlocks() {
+	GameRegistry.registerBlock(playerPlate);
+	GameRegistry.registerBlock(stackBench);
+	GameRegistry.registerBlock(machines, ItemBlockMachines.class);
+	GameRegistry.registerBlock(box, ItemBlockBox.class);
+	GameRegistry.registerBlock(box2, ItemBlockBox2.class);
+	GameRegistry.registerBlock(crate, ItemBlockCrate.class);
+	GameRegistry.registerBlock(crate2, ItemBlockCrate2.class);
+	GameRegistry.registerBlock(ores, ItemBlockOres.class);
+	GameRegistry.registerBlock(coloredGlass, ItemBlockGlass.class);
+	GameRegistry.registerBlock(beam, ItemBlockGlass.class);
+	GameRegistry.registerBlock(chimney, ItemBlockChimney.class);
+    }
+
+    /* Block names and translations */
+    public static void nameBlocks() {
+	playerPlate.setBlockName("pressurePlate");
+	stackBench.setBlockName("stackBench");
+	ores.setBlockName("ore");
+	box.setBlockName("box");
+	box2.setBlockName("box2");
+	crate.setBlockName("crate");
+	crate2.setBlockName("crate2");
+	machines.setBlockName("machine");
+	coloredGlass.setBlockName("coloredGlass");
+	beam.setBlockName("beam");
+	chimney.setBlockName("chimney");
+    }
 }
--- Common/si/meansoft/logisticraft/common/items/LCItems.java
+++ Common/si/meansoft/logisticraft/common/items/LCItems.java
@@ -22,53 +22,38 @@ import si.meansoft.logisticraft.common.library.BlockIDs;
 import si.meansoft.logisticraft.common.library.ItemIDs;
 
 public class LCItems {
-	
-	public static Item ingotCopper;
-	public static Item ingotSilver;
-	public static Item ingotPlatinum;
-	
-	public static Item coins;
-	public static Item dusts;
-	public static Item knife;
-	
-	public static ItemBlockOres ores;
-	
-	/* Item declarations */
-	public static void loadItems() {
-		ingotCopper = new ItemIngot(ItemIDs.ingotCopper).setIconIndex(18).setCreativeTab(CreativeTabs.tabMaterials);
-		ingotSilver = new ItemIngot(ItemIDs.ingotSilver).setIconIndex(17).setCreativeTab(CreativeTabs.tabMaterials);
-		ingotPlatinum = new ItemIngot(ItemIDs.ingotPlatinum).setIconIndex(16).setCreativeTab(CreativeTabs.tabMaterials);
-		
-		coins = new ItemCoins(ItemIDs.coins).setIconCoord(5, 0);
-		
-		dusts = new ItemDusts(ItemIDs.dusts).setIconIndex(19);
-		
-		EnumToolMaterial materialKnife = EnumHelper.addToolMaterial("Knife", 1, 50, 5.0F, 1, 7);
-		knife = new ItemKnife(ItemIDs.knife, materialKnife).setIconCoord(1, 0);
-	}
-	
-	/* Item names and translations */
-	public static void nameItems() {
-		ingotCopper.setItemName("copperIngot");
-		ingotSilver.setItemName("silverIngot");
-		ingotPlatinum.setItemName("platinumIngot");
-		knife.setItemName("knife");
-		dusts.setItemName("dusts");
-		coins.setItemName("coins");
-		
-		LanguageRegistry.addName(ingotCopper, "Copper Ingot");
-		LanguageRegistry.addName(ingotSilver, "Silver Ingot");
-		LanguageRegistry.addName(ingotPlatinum, "Platinum Ingot");
-		
-		LanguageRegistry.addName(new ItemStack(coins, 1, 0), "Copper coin");
-		LanguageRegistry.addName(new ItemStack(coins, 1, 1), "Silver coin");
-		LanguageRegistry.addName(new ItemStack(coins, 1, 2), "Gold coin");
-		LanguageRegistry.addName(new ItemStack(coins, 1, 3), "Platinum coin");
-		
-		LanguageRegistry.addName(new ItemStack(dusts, 1, 0), "Copper dust");
-		LanguageRegistry.addName(new ItemStack(dusts, 1, 1), "Silver dust");
-		LanguageRegistry.addName(new ItemStack(dusts, 1, 2), "Platinum dust");
-		
-		LanguageRegistry.addName(knife, "Knife");
-	}
+
+    public static Item ingotCopper;
+    public static Item ingotSilver;
+    public static Item ingotPlatinum;
+
+    public static Item coins;
+    public static Item dusts;
+    public static Item knife;
+
+    public static ItemBlockOres ores;
+
+    /* Item declarations */
+    public static void loadItems() {
+	ingotCopper = new ItemIngot(ItemIDs.ingotCopper).setIconIndex(18).setCreativeTab(CreativeTabs.tabMaterials);
+	ingotSilver = new ItemIngot(ItemIDs.ingotSilver).setIconIndex(17).setCreativeTab(CreativeTabs.tabMaterials);
+	ingotPlatinum = new ItemIngot(ItemIDs.ingotPlatinum).setIconIndex(16).setCreativeTab(CreativeTabs.tabMaterials);
+
+	coins = new ItemCoins(ItemIDs.coins).setIconCoord(5, 0);
+
+	dusts = new ItemDusts(ItemIDs.dusts).setIconIndex(19);
+
+	EnumToolMaterial materialKnife = EnumHelper.addToolMaterial("Knife", 1, 50, 5.0F, 1, 7);
+	knife = new ItemKnife(ItemIDs.knife, materialKnife).setIconCoord(1, 0);
+    }
+
+    /* Item names and translations */
+    public static void nameItems() {
+	ingotCopper.setItemName("copperIngot");
+	ingotSilver.setItemName("silverIngot");
+	ingotPlatinum.setItemName("platinumIngot");
+	knife.setItemName("knife");
+	dusts.setItemName("dust");
+	coins.setItemName("coin");
+    }
 }
