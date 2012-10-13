ded76aeed3efca454c4f3d40e00978b52b9e7e0b
Mrbrutal <jost.bizjak@gmail.com>
2012-10-14 01:33:12 +0200

    New items added.

---
+++ Common/si/meansoft/logisticraft/common/items/ItemBoards.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Mrbrutal. All rights reserved.
+ * 
+ * @name Logisticraft
+ * @author Mrbrutal
+ * @licence Lesser GNU Public License v3 http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+
+package si.meansoft.logisticraft.common.items;
+
+import si.meansoft.logisticraft.common.library.Info;
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.Item;
+
+public class ItemBoards extends Item{
+
+    protected ItemBoards(int par1) {
+	super(par1);
+	setMaxStackSize(64);
+	setCreativeTab(CreativeTabs.tabMaterials);
+	setTextureFile(Info.TEX_ITEM);
+    }
+
+}
