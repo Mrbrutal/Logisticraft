ca41ae8d11edacb815c156199b9b2f445c03533b
Mrbrutal <jost.bizjak@gmail.com>
2012-11-06 00:44:41 +0100

    Some formating.

--- Common/si/meansoft/logisticraft/common/core/CommonProxy.java
+++ Common/si/meansoft/logisticraft/common/core/CommonProxy.java
@@ -15,43 +15,53 @@ import si.meansoft.logisticraft.common.library.GuiIDs;
 import cpw.mods.fml.common.network.IGuiHandler;
 
 public class CommonProxy implements IGuiHandler {
-	public static CommonProxy proxy;
-	
-	public void registerKeyBindingHandler() {}
+    public static CommonProxy proxy;
 
-    public void setKeyBinding(String name, int value) {}
+    public void registerKeyBindingHandler() {
+    }
 
-    public void registerSoundHandler() {}
+    public void setKeyBinding(String name, int value) {
+    }
+
+    public void registerSoundHandler() {
+    }
 
     /* GUIS */
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-    	if(ID == GuiIDs.GUI_STACKBENCH) {
-    		return new ContainerStackbench(player.inventory, world, x, y, z);
-    	}
-    	else {
-    		return null;
-    	}
+	if (ID == GuiIDs.GUI_STACKBENCH) {
+	    return new ContainerStackbench(player.inventory, world, x, y, z);
+	}
+	else {
+	    return null;
+	}
     }
 
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-    	return null;
+	return null;
     }
 
     /* TEXTURE PRELOADS FROM CLIENT */
-	public void preloadTextures() {}
-	
-	/* LOCALIZATION */
-	public String getCurrentLanguage() {
-		return null;
-	}
-	
-	public void addName(Object obj, String s) {}
-	public void addLocalization(String s1, String string) {}
-	public String getItemDisplayName(ItemStack newStack) { return ""; }
+    public void preloadTextures() {
+    }
 
-	public World getClientWorld() {
-		return null;
-	}
+    /* LOCALIZATION */
+    public String getCurrentLanguage() {
+	return null;
+    }
+
+    public void addName(Object obj, String s) {
+    }
+
+    public void addLocalization(String s1, String string) {
+    }
+
+    public String getItemDisplayName(ItemStack newStack) {
+	return "";
+    }
+
+    public World getClientWorld() {
+	return null;
+    }
 }
